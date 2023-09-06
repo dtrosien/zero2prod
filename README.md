2af9b59b01d5c33dcdf14ea23560275f0e34300b
dtrosien <dtrosien@gmail.com>
2023-09-06 23:21:33 +0200

    up to chapter 3.11

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 email newsletter: a training project
 
-.env contains db information needed for compiling sqlx (see also comment in file)
+### structure
+* .env:  contains db information needed for compiling sqlx:
+  * sqlx reaches out to Postgres at compile-time to check that queries are well-formed. Just like sqlx-cli commands, it relies on the DATABASE_URL environment variable to know where to find the database.
+  * sqlx will read DATABASE_URL from it and save us the hassle of re-exporting the environment variable every single time.
+  * this is only needed for compiling, during runtime the configuration.yaml is used to change the db connection
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,18 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::{PgConnection, PgPool};
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // wrap connection in a smart pointer (uses Arc internally)
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
