d4dfe7dee8c92457988e88656e70d52a0a60c678
dtrosien <dtrosien@gmail.com>
2023-10-04 19:28:10 +0200

    up to chapter 5

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,29 @@
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use std::sync::OnceLock;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 mod common;
+
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+fn init_tracing() {
+    static TRACING: OnceLock<()> = OnceLock::new();
+    TRACING.get_or_init(|| {
+        let default_filter_level = "info".to_string();
+        let subscriber_name = "test".to_string();
+
+        if std::env::var("TEST_LOG").is_ok_and(|x| x.contains("true")) {
+            let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+            init_subscriber(subscriber);
+        } else {
+            let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+            init_subscriber(subscriber);
+        }
+    });
+}
 
 pub struct TestApp {
     pub address: String,
@@ -12,12 +32,14 @@ pub struct TestApp {
 
 // Launch the application in the background
 async fn spawn_app() -> TestApp {
+    init_tracing();
+
     // When defining port 0, the os will find a random free port
     let listener = TcpListener::bind("127.0.0.1:0").expect("could not bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
 
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
 
     let connection_pool = configure_database(&configuration.database).await;
@@ -33,16 +55,17 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
